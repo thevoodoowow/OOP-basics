c6a5b471292a4a9db58ec82213c11646f9fa2f39
voodoo <thevoodoowow>
2025-11-07 23:34:01 +0500

    completed zad2.py

---
+++ zad2.py
@@ -0,0 +1,18 @@
+class Button:
+    def __init__(self):
+        self.count = 0 
+    def click(self):
+        self.count += 1
+    def click_count(self):
+        return self.count
+    def reset(self):
+        self.count = 0
+# Ваш код
+
+button = Button()
+button.click()
+button.click()
+print(button.click_count())
+button.reset()
+button.click()
+print(button.click_count())
