4d4ff7c6df7ccc38a9af8ec492d58e9002bfcdf5
voodoo <thevoodoowow>
2025-11-09 20:47:49 +0500

    completed zad3

---
+++ zad3.py
@@ -0,0 +1,27 @@
+class Balance:
+    def __init__(self):
+        self.left_weight = 0
+        self.right_weight = 0
+
+    def add_left(self, weight):
+        self.left_weight += weight
+
+    def add_right(self, weight):
+        self.right_weight += weight
+
+    def result(self):
+        if self.left_weight > self.right_weight:
+            return "L"
+        elif self.right_weight > self.left_weight:
+            return "R"
+        else:
+            return "="
+# Ваш код
+
+balance = Balance()
+balance.add_right(10)
+balance.add_left(5)
+balance.add_left(5)
+print(balance.result())
+balance.add_left(5)
+print(balance.result())
