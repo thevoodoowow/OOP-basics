610e95909fa27de0f66bf747e74e7fd3bb803e9e
voodoo <thevoodoowow>
2025-11-09 21:53:46 +0500

    Inheritance zad1-2-3 completed

---
+++ Inheritance/zad2NASL.py
@@ -0,0 +1,26 @@
+class Alive:
+    pass
+
+class Acellularia(Alive):
+    pass
+
+class Cellularia(Alive):
+    pass
+
+class Prokaryota(Cellularia):
+    pass
+
+class Eukaryota(Cellularia):
+    pass
+
+class Unicellularia(Eukaryota):
+    pass
+
+class Fungi(Eukaryota):
+    pass
+
+class Plantae(Eukaryota):
+    pass
+
+class Animalia(Eukaryota):
+    pass
